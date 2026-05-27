b53019ad6726199862c5fc11be9c79f1a6997c1f
bryancv-super <bryancv2108@gmail.com>
2026-05-26 21:37:27 -0400

    refactor(frontend): restructure src architecture

---
+++ apps/frontend/src/app/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "BC Market App",
+  description: "",
+};
+
+export default function RootLayout() {
+  return (
+    <html
+      lang="en"
+    >
+      <body></body>
+    </html>
+  );
+}
